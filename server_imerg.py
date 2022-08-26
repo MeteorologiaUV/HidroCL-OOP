1d278c62c7707e6d71ac6adb1e281a0dd1e422a6
aldotapia <aatapia@userena.cl>
2022-08-25 21:52:21 -0400

    Fixed type

--- server_imerg.py
+++ server_imerg.py
@@ -6,9 +6,9 @@ pp = hidrocl.HidroCLVariable("pp",
                              hcl.pp_o_imerg_pp_mean_b_d_p0d,
                              hcl.pp_o_imerg_pp_mean_b_pc)
 
-mcd15 = hidrocl.Gpm_3imrghhl(pp,
+imerg = hidrocl.Gpm_3imrghhl(pp,
                              product_path=hcl.imerghhl_path,
                              vector_path=hcl.hidrocl_wgs84,
                              pp_log=hcl.log_pp_o_imerg_pp_mean)
 
-mcd15.run_extraction()
+imerg.run_extraction()
