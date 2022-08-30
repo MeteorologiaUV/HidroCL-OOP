5efee52245bb8b3d4d60bccbc01e9ffe9a63900c
aldotapia <aatapia@userena.cl>
2022-08-29 22:12:20 -0400

    Jobs added
    Added jobs for PERSIANN extraction

---
+++ server_persiann_ccs_cdr.py
@@ -0,0 +1,22 @@
+import hidrocl
+import hidrocl_paths as hcl
+import geopandas as gpd
+print(hidrocl.__version__)
+
+pp = hidrocl.HidroCLVariable("pp",
+                             hcl.pp_o_pcdr_pp_mean_b_d_p0d,
+                             hcl.pp_o_pcdr_pp_mean_b_pc)
+
+v = gpd.read_file(hcl.hidrocl_wgs84)
+catchment_names = v.gauge_id.tolist()
+
+pp.catchment_names = catchment_names
+
+pp.checkdatabase()
+pp.checkpcdatabase()
+
+persiann = hidrocl.Persiann_ccs_cdr(pp, product_path=hcl.persiann,
+                                    vector_path=hcl.hidrocl_wgs84,
+                                    pp_log=hcl.log_pp_o_pcdr_pp_mean)
+
+persiann.run_extraction()
