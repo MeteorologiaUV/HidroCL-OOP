4358b64964a32b45c907f84dc4e791cf486d296d
aldotapia <aatapia@userena.cl>
2022-08-30 17:41:57 -0400

    For cleaning product folder
    This may take long time, uploaded to server

---
+++ imerg_maintainer.py
@@ -0,0 +1,14 @@
+import hidrocl
+import hidrocl_paths as hcl
+print(hidrocl.__version__)
+
+pp = hidrocl.HidroCLVariable("pp",
+                             hcl.pp_o_imerg_pp_mean_b_d_p0d,
+                             hcl.pp_o_imerg_pp_mean_b_pc)
+
+imerg = hidrocl.Gpm_3imrghhl(pp,
+                             product_path=hcl.imerghhl_path,
+                             vector_path=hcl.hidrocl_wgs84,
+                             pp_log=hcl.log_pp_o_imerg_pp_mean)
+
+imerg.run_maintainer(hcl.log_file_maintainer)
