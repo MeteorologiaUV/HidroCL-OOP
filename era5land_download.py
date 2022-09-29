b8ca943b246e3239ee66384d119af7656bc9025f
aldotapia <aatapia@userena.cl>
2022-09-29 15:56:40 -0300

    improved loops for CDS data download

--- era5land_download.py
+++ era5land_download.py
@@ -3,25 +3,33 @@
 
 import os
 import hidrocl
+import pandas as pd
 import hidrocl_paths as hcl
 
 files = os.listdir(hcl.era5_land_hourly_path)
 
-for year in range(2000, 2023):
-    for month in range(1, 13):
-        for day in range(1, 32):
-            print(f'{year:04d}-{month:02d}-{day:02d}')
+start = '2000-01-01'
+end = '2022-10-01'
 
-            fname = f'era5-land_{year:04d}{month:02d}{day:02d}.nc'
+p = pd.period_range(pd.to_datetime(start, format="%Y-%m-%d"),
+                    pd.to_datetime(end, format="%Y-%m-%d"), freq='D')
 
-            if fname in files:
-                print('already downloaded')
-            else:
-                try:
-                    print('downloading')
-                    hidrocl.download.download_era5land(year=year,
-                                                       month=month,
-                                                       day=day,
-                                                       path=hcl.era5_land_hourly_path)
-                except Exception:
-                    print('day out of range')
+for i in p:
+    print(i)
+
+    year = int(i.strftime('%Y'))
+    month = int(i.strftime('%m'))
+    day = int(i.strftime('%d'))
+
+    fname = f'era5-land_{year:04d}{month:02d}{day:02d}.nc'
+    if fname in files:
+        print('already downloaded')
+    else:
+        try:
+            print('downloading')
+            hidrocl.download.download_era5land(year=year,
+                                               month=month,
+                                               day=day,
+                                               path=hcl.era5_land_hourly_path)
+        except Exception:
+            print('day out of range')
--- soil_moisture_download.py
+++ soil_moisture_download.py
@@ -3,26 +3,35 @@
 
 import os
 import hidrocl
+import pandas as pd
 import hidrocl_paths as hcl
 
 files = os.listdir(hcl.satellite_soil_moisture)
 files = list(set([file.split('-')[6][:8] for file in files]))
 
-for year in range(2000, 2023):
-    for month in range(1, 13):
-        for day in range(1, 32):
-            print(f'{year:04d}-{month:02d}-{day:02d}')
+start = '2000-01-01'
+end = '2022-10-01'
 
-            fname = f'{year:04d}{month:02d}{day:02d}'
+p = pd.period_range(pd.to_datetime(start, format="%Y-%m-%d"),
+                    pd.to_datetime(end, format="%Y-%m-%d"), freq='D')
 
-            if fname in files:
-                print('already downloaded')
-            else:
-                try:
-                    print('downloading')
-                    hidrocl.download.download_satsoilmoist(year=year,
-                                                           month=month,
-                                                           day=day,
-                                                           path=hcl.satellite_soil_moisture)
-                except Exception:
-                    print('day out of range')
+for i in p:
+    print(i)
+
+    year = int(i.strftime('%Y'))
+    month = int(i.strftime('%m'))
+    day = int(i.strftime('%d'))
+
+    fname = f'{year:04d}{month:02d}{day:02d}'
+
+    if fname in files:
+        print('already downloaded')
+    else:
+        try:
+            print('downloading')
+            hidrocl.download.download_satsoilmoist(year=year,
+                                                   month=month,
+                                                   day=day,
+                                                   path=hcl.satellite_soil_moisture)
+        except Exception:
+            print('day out of range')
