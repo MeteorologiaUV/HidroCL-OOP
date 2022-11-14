e9a3af0e9be29c2d7a732fe9218a444d4549c7cc
aldotapia <aatapia@userena.cl>
2022-11-14 09:01:23 -0300

    Added script to extract imerggis

---
+++ server_imerggis.py
@@ -0,0 +1,23 @@
+import hidrocl
+import hidrocl_paths as hcl
+import geopandas as gpd
+print(hidrocl.__version__)
+
+pp = hidrocl.HidroCLVariable("pp",
+                             hcl.pp_o_imerg_pp_mean_b_d_p0d,
+                             hcl.pp_o_imerg_pp_mean_b_pc)
+
+v = gpd.read_file(hcl.hidrocl_wgs84)
+catchment_names = v.gauge_id.tolist()
+
+pp.catchment_names = catchment_names
+
+pp.checkdatabase()
+pp.checkpcdatabase()
+
+imerg = hidrocl.ImergGIS(pp,
+                         product_path=hcl.imerghhl_path,
+                         vector_path=hcl.hidrocl_wgs84,
+                         pp_log=hcl.log_pp_o_imerg_pp_mean)
+
+imerg.run_extraction()
