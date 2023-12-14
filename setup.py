379edbdb649b38493b43ea72070bda6d3c93ceda
aldotapia <aatapia@userena.cl>
2023-12-14 09:24:05 -0300

    Setup conf updated + requirements added

--- setup.py
+++ setup.py
@@ -14,7 +14,14 @@ setup(
         'netCDF4>=1.6.0',
         'cdsapi>=0.5.1',
         'earthaccess>=0.8.1',
-        'python-cmr>=0.9.0',
+        'beautifulsoup4>=4.11.1',
+        'rasterio>=1.3.2',
+        'numpy>=1.23.2',
+        'xarray>=0.20.1',
+        'requests>=2.28.1',
+        'setuptools>=63.4.1',
+
+
     ],
     python_requires='>=3.10',
 )
