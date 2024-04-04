48d9aa4cc38909bc44f4cb69d9015c839fe2b89b
aldotapia <aatapia@userena.cl>
2024-04-04 15:44:16 -0300

    dotenv template edited
    Co-Authored-By: Christopher Paredes <24547950+cppa@users.noreply.github.com>

--- hidrocl/__init__.py
+++ hidrocl/__init__.py
@@ -27,30 +27,41 @@ def set_env(path = '.env'):
         None
     """
     # check if the path exists
-    if not os.path.exists(path):
-        raise FileNotFoundError(f'{path} not found')
+    global project_path, github_path, observed_products_path, forecasted_products_path,\
+        processing_path, hidrocl_root_path, report_emails
 
-    load_dotenv(path)
-    global project_path, github_path, observed_products_path, forecasted_products_path, processing_path, hidrocl_root_path
-    # check if the environment variables exists from the .env file
-    if 'PROJECT_PATH' not in os.environ:
-        raise KeyError('PROJECT_PATH not found in .env file')
-    if 'GITHUB_PATH' not in os.environ:
-        raise KeyError('GITHUB_PATH not found in .env file')
-    if 'OBSERVED_PRODUCTS_PATH' not in os.environ:
-        raise KeyError('OBSERVED_PRODUCTS_PATH not found in .env file')
-    if 'FORECASTED_PRODUCTS_PATH' not in os.environ:
-        raise KeyError('FORECASTED_PRODUCTS_PATH not found in .env file')
-    if 'PROCESSING_PATH' not in os.environ:
-        raise KeyError('PROCESSING_PATH not found in .env file')
-    if 'HIDROCL_ROOT_PATH' not in os.environ:
-        raise KeyError('HIDROCL_ROOT_PATH not found in .env file')
-    project_path = os.environ['PROJECT_PATH']
-    github_path = os.environ['GITHUB_PATH']
-    observed_products_path = os.environ['OBSERVED_PRODUCTS_PATH']
-    forecasted_products_path = os.environ['FORECASTED_PRODUCTS_PATH']
-    processing_path = os.environ['PROCESSING_PATH']
-    hidrocl_root_path = os.environ['HIDROCL_ROOT_PATH']
+    if not os.path.exists(path):
+        project_path = ''
+        github_path = ''
+        observed_products_path = ''
+        forecasted_products_path = ''
+        processing_path = ''
+        hidrocl_root_path = ''
+        report_emails = ['']
+
+    else:
+
+        load_dotenv(path)
+        # check if the environment variables exists from the .env file
+        if 'PROJECT_PATH' not in os.environ:
+            raise KeyError('PROJECT_PATH not found in .env file')
+        if 'GITHUB_PATH' not in os.environ:
+            raise KeyError('GITHUB_PATH not found in .env file')
+        if 'OBSERVED_PRODUCTS_PATH' not in os.environ:
+            raise KeyError('OBSERVED_PRODUCTS_PATH not found in .env file')
+        if 'FORECASTED_PRODUCTS_PATH' not in os.environ:
+            raise KeyError('FORECASTED_PRODUCTS_PATH not found in .env file')
+        if 'PROCESSING_PATH' not in os.environ:
+            raise KeyError('PROCESSING_PATH not found in .env file')
+        if 'HIDROCL_ROOT_PATH' not in os.environ:
+            raise KeyError('HIDROCL_ROOT_PATH not found in .env file')
+        project_path = os.environ['PROJECT_PATH']
+        github_path = os.environ['GITHUB_PATH']
+        observed_products_path = os.environ['OBSERVED_PRODUCTS_PATH']
+        forecasted_products_path = os.environ['FORECASTED_PRODUCTS_PATH']
+        processing_path = os.environ['PROCESSING_PATH']
+        hidrocl_root_path = os.environ['HIDROCL_ROOT_PATH']
+        report_emails = os.environ['REPORT_EMAILS'].split(',')
 
     return None
 
